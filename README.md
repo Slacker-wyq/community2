f7e478f77f53be6e81ce2e4be02e45d69bea4eb6
Slacker-wyq <2561079480@qq.com>
2019-06-08 13:00:44 +0800

    add h2 db

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 ##懒虫社区
 
-
 ##资料
 [spring文档 ](https://spring.io/guides)
 [Github OAuth Docs](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+[MySQL](https://www.runoob.com/)
+[Mybatis](http://www.mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure)
+[Spring data](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#spring-boot-starter-jdbc)
+
+
+##脚本
+```sql
+create table USER
+(
+  ID           INTEGER AUTO_INCREMENT primary key NOT NULL,
+  ACCOUNT_ID   VARCHAR(100),
+  NAME         VARCHAR(50),
+  TOKEN        CHAR(36),
+  GMT_CREATE   BIGINT,
+  GMT_MODIFIED BIGINT
+);
+```
--- src/main/java/com/slacker/community/controller/AuthorizeController.java
+++ src/main/java/com/slacker/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.slacker.community.controller;
 
 import com.slacker.community.dto.AccessTokenDTO;
 import com.slacker.community.dto.GithubUser;
+import com.slacker.community.mapper.UserMapper;
+import com.slacker.community.model.User;
 import com.slacker.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -19,9 +24,14 @@ public class AuthorizeController {
     private String client_secret;
     @Value("${github.redirect.url}")
     private String redirect_url;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -29,11 +39,24 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
         String token=githubProvider.getAccessToken(accessTokenDTO);
-        System.out.println(token);
-        GithubUser user=githubProvider.getUser(token);
-        System.out.println(user.getName());
+        GithubUser githubUser=githubProvider.getUser(token);
+        if(githubUser!=null){
+            User user=new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功,写cookie和session
+            request.getSession().setAttribute("user",githubUser);
 
-        return "index";
+            return "redirect:/";
+
+        }else{
+            //登录失败,重新登录
+            return "redirect:/";
+        }
     }
 
 }
